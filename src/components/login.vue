ecfbfa33ad85e1c87bfb8284e8f8380bc09e66b0
AbdAziz4212 <abdaziz140401@gmail.com>
2022-03-18 21:22:41 +0800

    add: Login Component

---
+++ src/components/login.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="login">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <br>
+        <input type="text" v-model="username" placeholder="Username">
+        <input type="password" v-model="password" placeholder="Password">
+        <button>Login</button>
+    </div>
+</template>
