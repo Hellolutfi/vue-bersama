da5726ed5b3fe0411ed762335c6278923280fca5
Alvin Faiz <softkopi3258@gmail.com>
2022-03-18 20:28:57 +0700

    Add Login

---
+++ src/components/LoginUser.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <form>
+            <input type="text" v-model="username" placeholder="Username">
+            <input type="password" v-model="password" placeholder="Password">
+            <button type="button" @click="login">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginUser',
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        login() {
+            console.log(this.username, this.password);
+        }
+    }
+}
+</script>
