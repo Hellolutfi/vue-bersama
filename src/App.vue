5f6d566124d8e2b76fadc1b9bddf9d0f2b42eb1e
Alvin Faiz <softkopi3258@gmail.com>
2022-03-18 21:08:31 +0700

    Add Register

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,27 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="register = true">Register</button>
+    <RegisterUser v-if="register"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import RegisterUser from './components/RegisterUser.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    RegisterUser
+  },
+  //var register = true
+  data(){
+    return {
+      register: false,
+    }
+  },
 }
 </script>
 
